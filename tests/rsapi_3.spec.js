b4ebd509c7aff5e03422d4a5551d5f3979b10901
Umesh kad <umeshkad2025@gmail.com>
2026-07-15 14:15:33 +0530

    failure trial

--- tests/rsapi_3.spec.js
+++ tests/rsapi_3.spec.js
@@ -3,7 +3,7 @@ import {test,expect} from '@playwright/test';
 test('my method',async ({request})=>{
 
     const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjIzMjAyLCJlbWFpbCI6InVtZXNoMTIzQGV4YW1wbGUuY29tIiwiaWF0IjoxNzg0MTA0MzAwLCJleHAiOjE3ODQ3MDkxMDB9.nRZ94GEJBpaZpXTVuTM-VPAvG-M96xBqjuRQcANjiYs';
-    const myResponse = await request.get('https://api.eventhub.rahulshettyacademy.com/api/events?category=Conference&city=Bangalore&search=summit&page=1&limit=10',
+    const myResponse = await .get('https://api.eventhub.rahulshettyacademy.com/api/events?category=Conference&city=Bangalore&search=summit&page=1&limit=10',
         {
             headers: {
               Authorization: `Bearer ${token}`,
